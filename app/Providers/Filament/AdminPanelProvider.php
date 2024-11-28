07d6131c89c08f431b4431643d4ffcbd149a9a78
Mitesh Viras <virashmitesh@gmail.com>
2024-11-28 22:35:00 +0530

    colors added.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,10 +31,16 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->plugins([
                 LightSwitchPlugin::make()
-                ->position(Alignment::BottomCenter),
+                    ->position(Alignment::BottomCenter),
             ])
             ->colors([
                 'primary' => Color::Amber,
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
+                'primary' => Color::Indigo,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
